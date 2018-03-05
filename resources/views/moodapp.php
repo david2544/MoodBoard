9dd382e8b31fd11ee8a2b1987ed91ff93b03222a
davidcoroian <david.coroian@code.berlin>
2018-03-05 10:22:05 +0100

    added result id

--- resources/views/moodapp.php
+++ resources/views/moodapp.php
@@ -3,6 +3,8 @@
 <head>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<title>Webhooks testing</title>
+<div id="result"></div>
+
 </head>
 <body>
 	<script>
