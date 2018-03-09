15b89d53fc0f05bf00c89cbd4798b94174524fe5
davidcoroian <david.coroian@code.berlin>
2018-03-09 19:57:33 +0100

    Added slack bot functionality with Laravel

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,33 @@ Route::get('/hook', 'BaseController@dump');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::post('/slack', function(\Illuminate\Http\Request $request){
+
+    $payload = $request->json();
+
+    if ($payload->get('type') === 'url_verification') {
+        return $payload->get('challenge');
+    }
+
+    // Bot logic will be placed here
+});
+
+Route::get('/login/slack', function(){
+    return Socialite::with('slack')
+        ->scopes(['bot'])
+        ->redirect();
+});
+Route::get('/connect/slack', function(\GuzzleHttp\Client $httpClient){
+    $response = $httpClient->post('https://slack.com/api/oauth.access', [
+        'headers' => ['Accept' => 'application/json'],
+        'form_params' => [
+            'client_id' => env('SLACK_KEY'),
+            'client_secret' => env('SLACK_SECRET'),
+            'code' => $_GET['code'],
+            'redirect_uri' => env('SLACK_REDIRECT_URI'),
+        ]
+    ]);
+    $bot_token = json_decode($response->getBody())->bot->bot_access_token;
+    echo "Your Bot Token is: ". $bot_token. " place it inside your .env as SLACK_TOKEN";
+});
