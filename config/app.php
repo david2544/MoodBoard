c485e7d9337eb6ca661af7c76b768ce93bdee3e6
davidcoroian <david.coroian@code.berlin>
2018-03-09 20:19:45 +0100

    removed bot since it didnt work :/

--- config/app.php
+++ config/app.php
@@ -150,7 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
-        \SocialiteProviders\Manager\ServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -208,7 +208,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Socialite' => \Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
