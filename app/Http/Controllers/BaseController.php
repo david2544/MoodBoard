d6b874dd03f6b127cabef05eaad7f1d87b0c3699
davidcoroian <david.coroian@code.berlin>
2018-03-09 21:02:10 +0100

    implementing outgoing webhooks

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -7,29 +7,47 @@ use Illuminate\Http\Request;
 
 class BaseController extends Controller
 {
-	function hook(Request $request) {
-		$payload = json_decode($request->get('payload'));
-		$mood_value = $payload->actions[0]->value;
-		$user_name = $payload->user->name;
-		$user_id = $payload->user->id;
-		$mood = new Mood;
-		$mood->mood_value = $mood_value;
-		$mood->user_name = $user_name;
-		$mood->user_id = $user_id;
-		$mood->save();
-	}
-	function outgoing(Request $request) {
-		$payload = json_decode($request->all());
-		$url = 'https://hooks.slack.com/services/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';
-		$ch = curl_init( $url );
-		curl_setopt( $ch, CURLOPT_POST, 1);
-		curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload);
-		curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-		curl_setopt( $ch, CURLOPT_HEADER, 0);
-		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+	function hook(Request $request)
+    {
+        if ($request->has('payload')) {
+            $payload = json_decode($request->get('payload'));
+            $mood_value = $payload->actions[0]->value;
+            $user_name = $payload->user->name;
+            $user_id = $payload->user->id;
+            $mood = new Mood;
+            $mood->mood_value = $mood_value;
+            $mood->user_name = $user_name;
+            $mood->user_id = $user_id;
+            $mood->save();
+        }
+        else{
+            $url = 'https://hooks.slack.com/services/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';
+            $payload = '{
+                "text": "African or European?"
+            }';
+            $ch = curl_init( $url );
+            curl_setopt( $ch, CURLOPT_POST, 1);
+            curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload);
+            curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt( $ch, CURLOPT_HEADER, 0);
+            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
 
-		$response = curl_exec( $ch );
-	}
+            $response = curl_exec( $ch );
+        }
+    }
+
+//	function outgoing(Request $request) {
+//		$payload = $request;
+//		$url = 'https://hooks.slack.com/services/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';
+//		$ch = curl_init( $url );
+//		curl_setopt( $ch, CURLOPT_POST, 1);
+//		curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload);
+//		curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+//		curl_setopt( $ch, CURLOPT_HEADER, 0);
+//		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+//
+//		$response = curl_exec( $ch );
+//	}
 
 	function dump() {
 		return Mood::all();
